6c45aaa2ba3213ca507a7e15d28881934ecd3371
fvafrCU <fvafrcu@arcor.de>
2017-07-06 16:32:20 +0200

    mess with cran comments

--- R/cran_comments.R
+++ R/cran_comments.R
@@ -1,28 +1,29 @@
-#!/usr/bin/Rscript --vanilla
-# search for  $ Rscript -e 'sessionInfo()' in the raw log of the travis build
-travis_copy <- c("
-R version 3.4.0 (2017-04-21)
-Platform: x86_64-pc-linux-gnu (64-bit)
-Running under: Ubuntu precise (12.04.5 LTS)
-")
-
-provide_cran_comments <- function(comments_file = "cran-comments.md",
-                                  check_log = "log/dev_check.Rout",
-                                  travis_raw_log = travis_copy) {
+provide_cran_comments <- function(check_log = "log/dev_check.Rout",
+                                  travis_raw_log = NULL) {
+    #FIXME: check_log without default!
+    if (is.null(travis_raw_log)) {
+            # search for  $ Rscript -e 'sessionInfo()' 
+            # in the raw log of the travis build
+            travis_raw_log <- c("
+                                R version 3.4.0 (2017-04-21)
+                                Platform: x86_64-pc-linux-gnu (64-bit)
+                                Running under: Ubuntu precise (12.04.5 LTS) 
+                                ")
+    }
     pkg <- devtools::as.package(".")
+    comments_file = file.path(pkg[["path"]], "cran-comments.md")
     cat("Dear CRAN Team,\n", 
-        "this is a resubmission of package ", pkg$package, ". I have\n", 
-        "XXX",
-        "\nBest, Dominik\n", sep = "", file = comments_file, 
+        "this is a resubmission of package ", pkg[["package"]], ". I have\n", 
+        "XXX", "\nBest, Dominik\n", sep = "", file = comments_file, 
         append = FALSE)
-    cat("\n# Package ", pkg$package, pkg$version, file = comments_file,
-        append = TRUE)
+    cat("\n# Package ", pkg[["package"]], pkg[["version"]], 
+        file = comments_file, append = TRUE)
     travis <- unlist(strsplit(travis_raw_log, "\n"))
     session <- utils::sessionInfo()
     here <- c("",
-              session$R.version$version.string,
-              paste0("Platform: ", session$platform),
-              paste0("Running under: ", session$running))
+              session[["R.version"]][["version.string"]],
+              paste0("Platform: ", session[["platform"]]),
+              paste0("Running under: ", session[["running"]]))
 
     cat("\n## Test  environments ", "\n", file = comments_file, append = TRUE)
     cat("-", paste(here[here != ""], collapse = "\n  "), "\n", 
