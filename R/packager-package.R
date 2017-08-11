691f89d9a9f13faab6210830d04022b93458f51f
fvafrCU <fvafrcu@arcor.de>
2017-08-11 15:30:45 +0200

    update DESCRIPTION and the package's documentation

---
+++ R/packager-package.R
@@ -0,0 +1,9 @@
+#' Helps Me Build Packages
+#'
+#' This is a set of wrappers around \pkg{devtools} and \pkg{MakefileR} and some
+#' sanity checks for developing packages.
+#' @name packager-package
+#' @aliases packager-package
+#' @docType package
+#' @keywords package
+NULL
