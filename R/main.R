39ba45524d16e1308300f54781566a41cf406188
fvafrCU <fvafrcu@arcor.de>
2017-09-22 13:40:34 +0200

    add option to disable the making of the new package, use ellipsis properly

--- R/main.R
+++ R/main.R
@@ -1,15 +1,15 @@
 create <- function(path, force = TRUE, ...) {
     if (isTRUE(force)) unlink(path, recursive = TRUE)
-    devtools::create(path = path, ...)
+    devtools::create(path = path, rstudio = FALSE, check = FALSE, quiet = TRUE)
     r <- git2r::init(path = path)
     paths <- unlist(git2r::status(r))
     git2r::add(r, paths)
     git2r::commit(r, "Initial Commit")
-    infect(path = path)
+    infect(path = path, ...)
     return(invisible(NULL))
 }
 
-infect <- function(path, ...) {
+infect <- function(path, make = TRUE, ...) {
     r <- git2r::init(path = path)
     devtools::use_build_ignore("^.*\\.tar\\.gz$", pkg = path, escape = FALSE)
     devtools::use_build_ignore(paste0(devtools::as.package(path)[["package"]], 
@@ -19,18 +19,14 @@ infect <- function(path, ...) {
     use_devel(path = path)
     remove_Rproj(path = path)
     use_devtools(path = path)
-    auth <- sub("(email)", "\n\t\\1", author_at_r("Andreas Dominik", "Cullmann", 
-                                      "fvafrcu@arcor.de"))
-    set_package_info(path = path, author_at_r = auth)
+    set_package_info(path = path, ...)
     use_bsd2clause_license(path = path)
-    substitution <- list("Authors@R" = auth)
-    document::alter_description_file(path = path, substitution = substitution)
     provide_throw(path = path)
     use_directory("log", pkg = path, ignore = TRUE)
     use_git_ignore("*.tar.gz", path = path)
     use_git_ignore(paste0(devtools::as.package(path)[["package"]], 
                                     ".Rcheck"), path = path)
-    if (length(Sys.which("make")) != 0) {
+    if (length(Sys.which("make")) != 0 && isTRUE(make)) {
         withr::with_dir(path, system2("make"))
     } else { # run at least roxygen
         roxygen2::roxygenize(package.dir = path)
--- devel.R
+++ devel.R
@@ -9,7 +9,12 @@ base::options(packager = list(could_force = TRUE, should_force = FALSE))
 base::getOption("packager")
 base::getOption("packager")[["could_force"]]
 devtools::load_all(".")
-create(path = "/tmp/foo", force = TRUE)
+create(path = "/tmp/foo", 
+       title = "Helps Me Build Packages", 
+       author_at_r = sub("(email)", "\n\t\\1", author_at_r("Andreas Dominik", "Cullmann", "fvafrcu@arcor.de")),
+       description = "This is a set of wrappers around `devtools` and `MakefileR` and some sanity checks for developing packages.",
+       details = "I find devtools very helpful, but not helpful enough.\nSo this is my highly personalised extension." ,
+       force = TRUE, make = FALSE)
 #
 if (FALSE) {
 packager::infect(path = ".",
