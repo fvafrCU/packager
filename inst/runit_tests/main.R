09f638161791ae04d461e8baddcd098552208a25
fvafrCU <fvafrcu@arcor.de>
2017-09-26 12:31:59 +0200

    enhance testing

--- inst/runit_tests/main.R
+++ inst/runit_tests/main.R
@@ -1,7 +1,20 @@
-notest_exception <- function() {
+notest_create <- function() {
     if (interactive()) devtools::load_all(".")
     d <- file.path(tempdir(), "prutp")
-    packager::create(path = d) 
+    packager::create(path = d, make = FALSE) 
+    l <- dir(d, recursive = TRUE, full.names = FALSE)
+    # checking on file contents will not work as timestamps and proc.time()
+    # recordings in files will change. 
+    result <- digest::sha1(l)
+    expected <- "76cb57ec226dd9ece66399bdc86939db59b96662"
+    RUnit::checkIdentical(result, expected, 
+                          msg = "Value of digest::sha1() differs!")
+}
+
+notest_create_make <- function() {
+    if (interactive()) devtools::load_all(".")
+    d <- file.path(tempdir(), "prutp")
+    packager::create(path = d, make = TRUE) 
     l <- dir(d, recursive = TRUE, full.names = FALSE)
     # checking on file contents will not work as timestamps and proc.time()
     # recordings in files will change. 
