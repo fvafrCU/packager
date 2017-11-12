4cfb05571832c6ca5aae9d7619213bc9d828eb06
fvafrCU <fvafrcu@arcor.de>
2017-11-12 21:15:50 +0100

    blow runit testing one nesting up

--- tests/runit.R
+++ tests/runit.R
@@ -13,11 +13,11 @@ if (interactive()) {
     unit_dir <- system.file("inst", "runit_tests", package = "packager")
 } else {
     r_call <- commandArgs(trailingOnly = FALSE)
+    require("packager", quietly=TRUE, character.only=TRUE) || 
+        stop("package '", pkgname, "' not found")
     if (any(grepl("--file", r_call))) {
         unit_dir <- file.path("inst", "runit_tests")
     } else {
-        require("packager", quietly=TRUE, character.only=TRUE) || 
-            stop("package '", pkgname, "' not found")
         unit_dir <- system.file("runit_tests", package = "packager")
     }
 }
