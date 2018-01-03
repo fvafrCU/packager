631f4ce88f041350644dcb2e5578a22f43c1eec2
fvafrCU <fvafrcu@arcor.de>
2018-01-03 12:54:13 +0100

    testing modified versions from devtools package

--- R/devtools_modified.R
+++ R/devtools_modified.R
@@ -126,14 +126,14 @@ use_travis <- function(path = ".", ...) {
 
 # devtools' version does not pass ceiling to git2r::discover_repository,
 # thus failing, if any .git found in the parents of the package path.
+# It also works only for R packages, which does not make much sense to me.
 use_git <- function(message = "Initial commit", path = ".") {
-    pkg <- devtools::as.package(path)
-    if (!is.null(git2r::discover_repository(pkg[["path"]], ceiling = 0))) {
+    if (! is.null(git2r::discover_repository(path, ceiling = 0))) {
         message("* Git is already initialized")
         return(invisible())
     }
     message("* Initialising repo")
-    r <- git2r::init(pkg[["path"]])
+    r <- git2r::init(path)
     use_git_ignore(c(".Rproj.user", ".Rhistory", ".RData"), path = path)
     message("* Adding files and committing")
     paths <- unlist(git2r::status(r))
@@ -156,11 +156,10 @@ union_write <- function(path, new_lines) {
 
 # Modified copy of devtools' unexported version
 use_git_ignore <- function(ignores, path = ".") {
-    pkg <- devtools::as.package(path)
     paths <- paste0("`", ignores, "`", collapse = ", ")
-    message("* Adding ", paths, " to ", file.path(pkg[["path"]],
+    message("* Adding ", paths, " to ", file.path(path,
         ".gitignore"))
-    path <- file.path(pkg[["path"]], ".gitignore")
+    path <- file.path(path, ".gitignore")
     union_write(path, ignores)
     invisible(TRUE)
 }
---
+++ inst/runit_tests/test_devtools_modified.R
@@ -0,0 +1,58 @@
+if (interactive()) devtools::load_all()
+
+provide_fake_package <- function() {
+    tmp <- tempfile()
+    dir.create(tmp)
+    path <- file.path(tmp, "fakePackage")
+    tryCatch(suppressMessages(devtools::create(path, quiet = TRUE)), 
+                              error = identity
+    )
+    return(path)
+}
+
+test_union_write <- function() {
+    path <- file.path(tempfile())
+    on.exit(unlink(path))
+    lines <- c("foo", "bar")
+    expectation <- lines
+    packager:::union_write(path, lines)
+    result <- readLines(path)
+    RUnit::checkIdentical(result, expectation)
+    line <- "foo bar"
+    expectation <- c(lines, line)
+    packager:::union_write(path, line)
+    result <- readLines(path)
+    RUnit::checkIdentical(result, expectation)
+}
+
+test_use_git_ignore <- function() {
+    expectation <- TRUE
+    path <- file.path(tempdir(), "fakePackge")
+    dir.create(path)
+    on.exit(unlink(path, recursive = TRUE))
+    git2r::init(path)
+    ignores <- c("foo", "bar")
+    result <- packager:::use_git_ignore(path = path, ignores = ignores)
+    RUnit::checkIdentical(result, expectation)
+
+    expectation <- ignores
+    result  <- readLines(file.path(path, ".gitignore"))
+    RUnit::checkIdentical(result, expectation)
+
+}
+
+test_use_git <- function() {
+    expectation <- TRUE
+    path <- file.path(tempdir(), "fakePackge")
+    dir.create(path)
+    on.exit(unlink(path, recursive = TRUE))
+    result <- packager:::use_git(path)
+    RUnit::checkIdentical(result, expectation)
+
+    # rerun on intialised git repo
+    expectation <- NULL
+    result <- packager:::use_git(path)
+    RUnit::checkIdentical(result, expectation)
+}
+
+
