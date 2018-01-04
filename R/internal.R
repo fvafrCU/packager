65a2918f1fc88f823c182fd5d8b11954975c1d3c
fvafrCU <fvafrcu@arcor.de>
2018-01-04 15:15:20 +0100

    add internal function git_add_commit()
    Didn't find git2r equivalent to git commit -am"XXX",
    git2r::commit(all = TRUE) doesn't seem to do the job.

--- R/internal.R
+++ R/internal.R
@@ -230,3 +230,15 @@ travis_cli <- function(path) {
     travis_session_info <- travis_log[(k + 1): (k + 3)]
     return(travis_session_info)
 }
+
+#' Git Add All Changes and Commit
+#'
+#' The same as git commit -am"M", where M is the \code{message}.
+#' @param repo The repository to commit.
+#' @param message The commit message to use.
+#' @return The return value of \code{\link{gitr::commit}}.
+git_add_commit <- function(repo, message = "Uncommented Changes: Backing Up") {
+    git2r::add(repo, unlist(git2r::status(repo)))
+    return(git2r::commit(repo, message = message))
+
+}
