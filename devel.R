78ff93b037c4d966a630d38c7b8178dea5e37e8b
fvafrCU <fvafrcu@arcor.de>
2018-01-23 17:47:33 +0100

    Uncommented changes: backing up

--- devel.R
+++ devel.R
@@ -1,5 +1,6 @@
+devtools::use_dev_version()
 devtools::load_all(".")
-provide_cran_comments(check_log = "log/check.Rout", travis_session_info = "travis-cli")
+packager::provide_cran_comments(check_log = "log/check.Rout", travis_session_info = "travis-cli")
 if (FALSE) {
     set_package_info(path = ".", title = "Helps Me Build Packages", 
                      description = paste("Helper functions for a build system,",
@@ -12,37 +13,16 @@ if (FALSE) {
                      force = is_force()
                      )
 }
-package <- "fvafrcu1"
-root <- file.path("", "tmp")
-path <- file.path(root, package)
-devtools::create(path)
-utils:::package.skeleton(package, path = root)
 
-
-base::options(packager = list(could_force = TRUE, should_force = FALSE))
-base::getOption("packager")
-base::getOption("packager")[["could_force"]]
-devtools::load_all(".")
-packager::provide_throw()
 # test logic
 devtools::load_all(".")
 d <- "/tmp/foo"
 packager::create(path = d) 
 l <- dir(d, recursive = TRUE, full.names = TRUE)
 digest::sha1(c(l, sapply(l, readLines)))
-#
-if (FALSE) {
-    packager::infect(path = ".",
-                     title = "Helps Me Build Packages", 
-                     author_at_r = sub("(email)", "\n\t\\1", author_at_r("Andreas Dominik", "Cullmann", "fvafrcu@arcor.de")),
-                     description = "This is a set of wrappers around `devtools` and `MakefileR` and some sanity checks for developing packages.",
-                     details = "I find devtools very helpful, but not helpful enough.\nSo this is my highly personalised extension." ,
-                     make = FALSE, git_add_and_commit = FALSE)
-}
 
-
-devtools::load_all(".")
 # release
+devtools::load_all(".")
 path = "."
 stop_on_git = TRUE
 
