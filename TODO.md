ee2d4f41ac26f228585a9ae40845533030b1d0c9
fvafrCU <fvafrcu@arcor.de>
2018-01-23 18:06:57 +0100

    Reset url in README according to current branch

--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
-- enhance coverage
-- document the options (force)
 - use fakemake::make in infect with a makelist that's an extension of fakemake's
   "package" list
 - add a make.R (using cmdline args) as template
+- document the options (force)
+- enhance coverage
 - devel the release() function
 - add release() conditionally to makelist.
 - fix todo in git\_add\_commit() (git2r::status)
+- try to re-implement use digest::sha1() for unit testing.
--- devel.R
+++ devel.R
@@ -14,28 +14,27 @@ if (FALSE) {
                      )
 }
 
-# test logic
-devtools::load_all(".")
-d <- "/tmp/foo"
-packager::create(path = d) 
-l <- dir(d, recursive = TRUE, full.names = TRUE)
-digest::sha1(c(l, sapply(l, readLines)))
+if (FALSE) {
+    # release
+    devtools::load_all(".")
+    path = "."
+    stop_on_git = TRUE
 
-# release
-devtools::load_all(".")
-path = "."
-stop_on_git = TRUE
-
-if (uses_git(path) && isTRUE(stop_on_git)) {
+    if (uses_git(path) && isTRUE(stop_on_git)) {
         if (is_git_uncommitted(path = path) )
             throw("You have uncommitted changes.")
         if (! git_sync_status(path = path)) 
             throw("Your repository is not synced with it's upstream.")
+    }
+    csu <- "http://xmpalantir.wu.ac.at/cransubmit/index2.php"
+    built_path <- build_cran(path, args = NULL)
+    if (yesno("Ready to submit?")) {
+        throw("User request.")
+    } else {
+        upload_cran(pkg = path, built_path = built_path, cran_submission_url = csu)
+    }
 }
-csu <- "http://xmpalantir.wu.ac.at/cransubmit/index2.php"
-built_path <- build_cran(path, args = NULL)
-if (yesno("Ready to submit?")) {
-    throw("User request.")
-} else {
-    upload_cran(pkg = path, built_path = built_path, cran_submission_url = csu)
-}
+
+# make
+fml <- fakemake::provide_make_list("package")
+fakemake::make("check", fml, verbose = FALSE)
