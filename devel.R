9e0e960e489d5288402a76db47da847aa6b656b4
fvafrCU <fvafrcu@arcor.de>
2018-04-12 12:22:09 +0200

    devel

--- devel.R
+++ devel.R
@@ -1,7 +1,7 @@
 devtools::load_all(".")
 devtools::use_dev_version()
 if (FALSE) {
-packager::provide_cran_comments(check_log = "log/check.Rout", travis_session_info = "travis-cli")
+    packager::provide_cran_comments(check_log = "log/check.Rout", travis_session_info = "travis-cli")
     set_package_info(path = ".", title = "Helps Me Build Packages", 
                      description = paste("Helper functions for a build system,",
                                          "heavily borrowing from and extending", 
@@ -28,14 +28,64 @@ packager::create("../cuwintest")
 
 
 # ============ cyclo deps
-#' log_file = file.path("~", "git", "os", "fva", "bwibw", "log", "cyclocomp.Rout") 
-update_cyclocomp_deps <- function(log) {
-    log <- readLines(log_file)
-    if (any(grepl("packages:", log))) {
-        line <- grep("packages:", log, value = TRUE)
-        packages <- trimws(unlist(strsplit(strsplit(line, split = ":")[[1]][2], 
-                                           split = ",")))
-        update.packages(oldPkgs = packages, ask = FALSE)
+
+# from remotes package version 1.1.1
+has_package <- function(pkg) {
+    if (pkg %in% loadedNamespaces()) {
+        TRUE
+    }
+    else {
+        requireNamespace(pkg, quietly = TRUE)
     }
 }
 
+safe_install_packages <- function(...) {
+    lib <- paste(.libPaths(), collapse = ":")
+    if (has_package("crancache") && has_package("callr")) {
+        i.p <- "crancache" %::% "install_packages"
+    } else {
+        i.p <- utils::install.packages
+    }
+    withr::with_envvar(c(R_LIBS = lib, R_LIBS_USER = lib, R_LIBS_SITE = lib, 
+                         R_PROFILE_USER = tempfile()), i.p(...))
+}
+
+install_packages <- function(packages, repos = getOption("repos"), 
+                             type = getOption("pkgType"), 
+                             ..., dependencies = FALSE, quiet = NULL) {
+    if (identical(type, "both")) 
+        type <- "binary"
+    if (is.null(quiet)) 
+        quiet <- !identical(type, "source")
+    message("Installing ", length(packages), " packages: ", paste(packages, 
+                                                                  collapse = ", "))
+    safe_install_packages(packages, repos = repos, type = type, 
+                          ..., dependencies = dependencies, quiet = quiet)
+}
+update <- function(object, ..., quiet = FALSE, upgrade = TRUE) {
+    ahead <- object$package[object$diff == 2L]
+    if (length(ahead) > 0 && !quiet) {
+        message("Skipping ", length(ahead), " packages not available: ", 
+                paste(ahead, collapse = ", "))
+    }
+    missing <- object$package[object$diff == 1L]
+    if (length(missing) > 0 && !quiet) {
+        message("Skipping ", length(missing), " packages ahead of CRAN: ", 
+                paste(missing, collapse = ", "))
+    }
+    if (upgrade) {
+        behind <- object$package[object$diff < 0L]
+    } else {
+        behind <- object$package[is.na(object$installed)]
+    }
+    if (length(behind) > 0L) {
+        install_packages(behind, repos = attr(object, "repos"), 
+                         type = attr(object, "type"), ...)
+    }
+}
+
+update_deps <- function(path) {
+    deps <- remotes::dev_package_deps(path)
+    update(deps)
+}
+update_deps(".")
