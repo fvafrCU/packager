b82f6e59889be57207b2787ea8c2b7d0422a3501
fvafrCU <fvafrcu@arcor.de>
2018-04-26 12:08:44 +0200

    Uncommented changes: backing up

--- R/dev_version.R
+++ R/dev_version.R
@@ -1,8 +1,21 @@
+use_dev_version <- function(path) {
+    devtools::use_dev_version(pkg = path)
+    status <- use_dev_news(path = path)
+    return(status)
+}
+
+
 use_dev_news <- function(path = ".") {
     news_file <- file.path(path, "NEWS.md")
-    news <- readLines(news_file)
-    news <- c(paste("#", desc::desc_get("Package"), desc::desc_get_version()),
-              "", "* FIXME", "",
-              news)
-    writeLines(news, news_file)
+    if (! file.exists(news_file)) {
+        status = FALSE
+    } else {
+        news <- readLines(news_file)
+        dev_paragraph <- paste("#", desc::desc_get("Package"), 
+                               desc::desc_get_version())
+        news <- c(dev_paragraph, "", "* FIXME", "", news)
+        writeLines(news, news_file)
+        status  <- TRUE
+    }
+    return(status)
 }
